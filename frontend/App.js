f2ddd4efb42875215cd41f938e619225f4748797
uthrasriram <143797369+uthrasriram@users.noreply.github.com>
2026-04-14 12:37:00 +0530

    Create main entry point for React Native Application

---
+++ frontend/App.js
@@ -0,0 +1,9 @@
+// Entry point for React Native Application
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+
+const App = () => {
+  return <SafeAreaView>{/* Your Application Code */}</SafeAreaView>;
+};
+
+export default App;
